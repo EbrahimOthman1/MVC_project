665ff2b6ab037bd9cb9a2a8aa833d7e0c9ad75ec
ebrahimothman1 <ebrahimothman1996@gmail.com>
2019-11-11 19:37:23 +0200

    MVC

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'ebrahimotman');
+  define('DB_PASS', '123');
+  define('DB_NAME', 'posts');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/shareposts');
+  // Site Name
+  define('SITENAME', 'SharePosts');
+  // App Version
+  define('APPVERSION', '1.0.0');
